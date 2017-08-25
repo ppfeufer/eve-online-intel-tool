10ddb2e5b1d9cf795a04a4fb5e2470c6a635b7cc
H.-Peter Pfeufer <info@ppfeufer.de>
2017-08-25 23:05:06 +0200

    Added "New D-Scan" button

--- templates/single-dscan.php
+++ templates/single-dscan.php
@@ -31,7 +31,10 @@ $countOffGrid = (!empty($dscanDataOffGrid['count'])) ? $dscanDataOffGrid['count'
 	?>
 
 	<div class="eve-intel-copy-to-clipboard copy-permalink-to-clipboard">
-		<ul class="nav nav-pills nav-stacked">
+		<ul class="nav nav-pills clearfix">
+			<li role="presentation">
+				<a href="<?php echo \get_post_type_archive_link('intel'); ?>"><span type="button" class="btn btn-default"><?php echo \__('New D-Scan', 'eve-online-intel-tool') ; ?></span></a>
+			</li>
 			<li role="presentation">
 				<span type="button" class="btn btn-default btn-copy-permalink-to-clipboard" data-clipboard-action="copy" data-clipboard-text="<?php echo \get_the_permalink(); ?>"><?php echo \__('Copy Permalink', 'eve-online-intel-tool') ; ?></span>
 			</li>
