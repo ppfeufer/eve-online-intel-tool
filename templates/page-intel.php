94a5356ff89db791013ca4048de0492ab8b361c0
Peter Pfeufer <p.pfeufer@thamm-it.de>
2017-09-13 15:12:22 +0200

    Issue a warning when intel couldn't be parsed

--- templates/page-intel.php
+++ templates/page-intel.php
@@ -3,9 +3,10 @@
  * Template Name: EVE Intel
  */
 
-$serverRequest = \filter_input(INPUT_SERVER, 'REQUEST_METHOD');
-$formAction = \filter_input(INPUT_POST, 'action');
+$serverRequest = \filter_input(\INPUT_SERVER, 'REQUEST_METHOD');
+$formAction = \filter_input(\INPUT_POST, 'action');
 
+$failedIntel = false;
 if($serverRequest === 'POST' && !empty($formAction) && $formAction === 'new_intel') {
 	$parsedIntel = new \WordPress\Plugin\EveOnlineIntelTool\Libs\IntelParser;
 
@@ -14,6 +15,8 @@ if($serverRequest === 'POST' && !empty($formAction) && $formAction === 'new_inte
 
 		\wp_redirect($link);
 	} // END if($parsedIntel->postID !== null)
+
+	$failedIntel = true;
 } // END if($serverRequest === 'POST' && !empty($formAction) && $formAction === 'new_intel')
 
 \get_header();
@@ -31,13 +34,25 @@ if($serverRequest === 'POST' && !empty($formAction) && $formAction === 'new_inte
 						<?php echo \__('Please keep in mind, parsing large amount of data can take some time. Be patient, CCP\'s API is not the fastest to answer ....', 'eve-online-intel-tool'); ?>
 					</p>
 					<div class="row">
-						<div class="col-lg-4 paste-explanation">
-							<header class="entry-header"><h2 class="entry-title"><?php echo \__('What can I paste?', 'eve-online-intel-tool'); ?></h2></header>
-							<ul>
-								<li>» <?php echo \__('D-Scan', 'eve-online-intel-tool'); ?></li>
-								<li>» <?php echo \__('Chat Memberlist', 'eve-online-intel-tool'); ?> <small><?php echo \__('(takes some time to parse)', 'eve-online-intel-tool'); ?></small></li>
-								<li>» <?php echo \__('Fleet Composition', 'eve-online-intel-tool'); ?> <small><?php echo \__('(takes some time to parse)', 'eve-online-intel-tool'); ?></small></li>
-							</ul>
+						<div class="col-lg-4">
+							<?php
+							if($failedIntel === true) {
+								?>
+								<div class="bs-callout bs-callout-warning intel-parse-error">
+									<p class="small"><?php echo \__('The provided data could not be parsed. Please check and try again.', 'eve-online-intel-tool'); ?></p>
+								</div>
+								<?php
+							}
+							?>
+
+							<div class="paste-explanation">
+								<header class="entry-header"><h2 class="entry-title"><?php echo \__('What can I paste?', 'eve-online-intel-tool'); ?></h2></header>
+								<ul>
+									<li>» <?php echo \__('D-Scan', 'eve-online-intel-tool'); ?></li>
+									<li>» <?php echo \__('Chat Memberlist', 'eve-online-intel-tool'); ?> <small><?php echo \__('(takes some time to parse)', 'eve-online-intel-tool'); ?></small></li>
+									<li>» <?php echo \__('Fleet Composition', 'eve-online-intel-tool'); ?> <small><?php echo \__('(takes some time to parse)', 'eve-online-intel-tool'); ?></small></li>
+								</ul>
+							</div>
 
 							<div class="bs-callout bs-callout-info">
 								<p class="small"><?php echo \sprintf(\__('This tool is still in its testing phase, so it might have some hick ups and rough edges here and there. If you experience any odd behaviour, please let me know about it in the %1$s.', 'eve-online-intel-tool'), '<a href="' .\esc_url('https://github.com/ppfeufer/eve-online-intel-tool/issues') . '" rel="external">' . \__('Github Issue Tracker', '') . '</a>'); ?></p>
