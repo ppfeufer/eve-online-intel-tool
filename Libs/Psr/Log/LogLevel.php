795755db97d7e0a6f2ee5565f0c8101270e23558
H.-Peter Pfeufer <info@ppfeufer.de>
2017-12-25 00:31:18 +0100

    Codeformatting and namespacing Signed-off-by:H.-Peter Pfeufer <info@ppfeufer.de>

--- Libs/Psr/Log/LogLevel.php
+++ Libs/Psr/Log/LogLevel.php
@@ -5,14 +5,14 @@ namespace WordPress\Plugin\EveOnlineIntelTool\Libs\Psr\Log;
 /**
  * Describes log levels.
  */
-class LogLevel
-{
-    const EMERGENCY = 'emergency';
-    const ALERT     = 'alert';
-    const CRITICAL  = 'critical';
-    const ERROR     = 'error';
-    const WARNING   = 'warning';
-    const NOTICE    = 'notice';
-    const INFO      = 'info';
-    const DEBUG     = 'debug';
+class LogLevel {
+	const EMERGENCY = 'emergency';
+	const ALERT = 'alert';
+	const CRITICAL = 'critical';
+	const ERROR = 'error';
+	const WARNING = 'warning';
+	const NOTICE = 'notice';
+	const INFO = 'info';
+	const DEBUG = 'debug';
+
 }
--- Libs/Psr/Log/LoggerAwareTrait.php
+++ Libs/Psr/Log/LoggerAwareTrait.php
@@ -5,22 +5,20 @@ namespace WordPress\Plugin\EveOnlineIntelTool\Libs\Psr\Log;
 /**
  * Basic Implementation of LoggerAwareInterface.
  */
-trait LoggerAwareTrait
-{
-    /**
-     * The logger instance.
-     *
-     * @var LoggerInterface
-     */
-    protected $logger;
+trait LoggerAwareTrait {
+	/**
+	 * The logger instance.
+	 *
+	 * @var LoggerInterface
+	 */
+	protected $logger;
 
-    /**
-     * Sets a logger.
-     *
-     * @param LoggerInterface $logger
-     */
-    public function setLogger(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
+	/**
+	 * Sets a logger.
+	 *
+	 * @param LoggerInterface $logger
+	 */
+	public function setLogger(LoggerInterface $logger) {
+		$this->logger = $logger;
+	}
 }
