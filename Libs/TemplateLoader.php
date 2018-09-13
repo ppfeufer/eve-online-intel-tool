3d980b167bca516e67862be8e3f22d40407f2d24
Peter Pfeufer <ppfeufer@gmail.com>
2018-09-13 11:09:57 +0200

    Some more codestyle fixes

--- Libs/TemplateLoader.php
+++ Libs/TemplateLoader.php
@@ -52,13 +52,13 @@ class TemplateLoader {
     /**
      * Adds our template to the page dropdown for v4.7+
      *
-     * @param array $posts_templates
+     * @param array $postsTemplates
      * @return array
      */
-    public function addNewTemplate($posts_templates) {
-        $posts_templates = \array_merge($posts_templates, $this->templates);
+    public function addNewTemplate($postsTemplates) {
+        $postsTemplates = \array_merge($postsTemplates, $this->templates);
 
-        return $posts_templates;
+        return $postsTemplates;
     }
 
     /**
@@ -70,7 +70,7 @@ class TemplateLoader {
      */
     public function registerProjectTemplates($atts) {
         // Create the key used for the themes cache
-        $cache_key = 'page_templates-' . \md5(\get_theme_root() . '/' . \get_stylesheet());
+        $cacheKey = 'page_templates-' . \md5(\get_theme_root() . '/' . \get_stylesheet());
 
         // Retrieve the cache list.
         // If it doesn't exist, or it's empty prepare an array
@@ -81,7 +81,7 @@ class TemplateLoader {
         }
 
         // New cache, therefore remove the old one
-        \wp_cache_delete($cache_key, 'themes');
+        \wp_cache_delete($cacheKey, 'themes');
 
         // Now add our template to the list of templates by merging our templates
         // with the existing templates array from the cache.
@@ -89,7 +89,7 @@ class TemplateLoader {
 
         // Add the modified cache to allow WordPress to pick it up for listing
         // available templates
-        \wp_cache_add($cache_key, $templates, 'themes', 1800);
+        \wp_cache_add($cacheKey, $templates, 'themes', 1800);
 
         return $atts;
     }
