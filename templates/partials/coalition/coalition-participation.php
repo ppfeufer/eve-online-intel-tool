a7dc72571d94dc9d4fd36f0e1f855dd606cdf876
Peter Pfeufer <info@ppfeufer.de>
2019-04-29 23:16:21 +0200

    Code formatting

--- templates/partials/coalition/coalition-participation.php
+++ templates/partials/coalition/coalition-participation.php
@@ -29,7 +29,7 @@ if(!empty($coalitionParticipation)) {
     <div class="row">
         <div class="col-md-12">
             <div class="progress">
-            <?php
+                <?php
                 foreach($coalitionParticipation['coalition'] as $coalitionNumbers) {
                     foreach($coalitionNumbers as $coalition) {
                         ?>
